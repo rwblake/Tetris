f2812c62eee02caf7b5d2aac75189b67949fbbf4
Robert Blake <rwblake@gmail.com>
2020-07-15 17:17:24 +0100

    added scoring

--- game_screen.py
+++ game_screen.py
@@ -10,18 +10,23 @@ import tetrimino as ttr
 class Game:
 	size = [10, 20]
 	speed_ms = 300
+	scoring = {0: 0, 1: 100, 2: 400, 3: 900, 4:2000}
 
 	def __init__(self, parent, scale):
 		self.parent = parent
 		self.scale = scale
 
+		self.end = False
+		self.score = 0
+		self.soft_lines = 0
+
 		self.grid = np.zeros(self.size, dtype=bool)
 		self.canvas = tk.Canvas(self.parent, width=self.size[0] * 32 * self.scale, height=self.size[1] * 32 * self.scale, bg='black', highlightthickness=0)
 		self.canvas.pack()
-		self.end = False
+		self.score_text = self.canvas.create_text(self.size[0]*32, 0, anchor='ne', text=str(self.score), fill='white')
 
 		self.static_blocks = np.empty(self.size, dtype=int)
-
+		self.squares = 0
 		self.t = ttr.random_tetrimino(self.grid)
 		self.t_drawn = self.draw_ttr(self.t)
 
@@ -45,6 +50,7 @@ class Game:
 		elif key == 'Down':
 			self.t.fall()
 			self.redraw()
+			self.soft_lines += 1
 			if self.t.locked:
 				self.new_ttr()
 				if self.t.locked:
@@ -80,6 +86,7 @@ class Game:
 				self.canvas.delete(self.static_blocks[i, row])
 				self.static_blocks[i, row] = 99999
 				self.grid[i, row] = False
+				self.squares -= 1
 		for y in range(row - 1, 0, -1):
 			for x in range(self.size[0]):
 				if self.grid[x, y]:
@@ -93,13 +100,23 @@ class Game:
 		for pos, t in zip(self.t.positions, self.t_drawn):
 			self.grid[tuple(pos)] = True
 			self.static_blocks[tuple(pos)] = t
+			self.squares += 1
 		self.t = ttr.random_tetrimino(self.grid)
 		self.t_drawn = []
 
 		row = self.full_row()
+		lines = 0
 		while row is not None:
 			self.delete_and_shift(row)
+			lines += 1
 			row = self.full_row()
+
+		added_score = self.soft_lines + self.scoring[lines]
+		if self.squares == 0:
+			added_score *= 10
+		self.score += added_score
+
+		self.canvas.itemconfig(self.score_text, text=str(self.score))
 
 	def loop(self):
 		if self.end:
